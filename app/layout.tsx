3c3f769272daaec99cf69720f8fe34087cef6e98
MattressAIBlake <blake@themattressai.com>
2024-11-02 14:58:38 -0500

    updated tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <ToastContainer />
+        <Analytics />
       </body>
     </html>
   )
